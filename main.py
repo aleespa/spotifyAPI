ab357666508d43eaede8e5d4e43630ebe299dc4c
Alejandro López <aleespazx@gmail.com>
2023-12-03 18:55:29 +0000

    iterate over a list of years

--- main.py
+++ main.py
@@ -21,20 +21,20 @@ while results:
     tracks.extend(results['items'])
     results = sp.next(results)
 
-specific_year = "1968"  # Replace with the desired year
-filtered_tracks = []
+for specific_year in ["1968" "1970", "1980", '1990', "2018", "2019",'2020', '2021', '2022', '2023']:
+    filtered_tracks = []
 
-for item in tracks:
-    track = item['track']
-    release_year = track['album']['release_date'][:4]
-    if release_year == specific_year:
-        filtered_tracks.append(track)
+    for item in tracks:
+        track = item['track']
+        release_year = track['album']['release_date'][:4]
+        if release_year == specific_year:
+            filtered_tracks.append(track)
 
+    playlist_name = f"Playlist {specific_year}"
+    playlist_description = "Created with Python and Spotipy!"
+    user_id = sp.current_user()['id']
+    playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description=playlist_description)
 
-playlist_name = "My New Playlist"
-playlist_description = "Created with Python and Spotipy!"
-user_id = sp.current_user()['id']
-playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description=playlist_description)
-
-playlist_id = playlist['id']
-sp.playlist_add_items(playlist_id, filtered_tracks)
+    print(filtered_tracks)
+    playlist_id = playlist['id']
+    sp.playlist_add_items(playlist_id, [track['uri'] for track in filtered_tracks])
